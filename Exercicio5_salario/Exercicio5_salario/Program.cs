e60bbb8c74bbc6eaa20f3773b1fb567a180bf04b
Daiane Costa <cicuto.adv@gmail.com>
2024-04-22 03:53:38 -0300

    add exercicio5

---
+++ Exercicio5_salario/Exercicio5_salario/Program.cs
@@ -0,0 +1,14 @@
+﻿//Programa que le o salario de uma pessoa e o valor com acrescimo e informa a porcentagem do acrescimo. 
+
+double sal, salacrescimo, diferenca, porcentagem;
+Console.WriteLine("Digite o salario");
+sal = double.Parse (Console.ReadLine());
+Console.WriteLine ("Digite o salario com acrescimo");
+salacrescimo = double.Parse(Console.ReadLine());
+
+diferenca = salacrescimo - sal;
+porcentagem = (diferenca / sal) * 100;
+
+Console.WriteLine($"A porcentagem de acrescimo salarial é de: % " + porcentagem);
+    
+
