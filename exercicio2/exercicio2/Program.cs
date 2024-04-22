adc9e14a7101f7039da7af7e2fc4ff6e0864137a
Daiane Costa <cicuto.adv@gmail.com>
2024-04-21 23:44:44 -0300

    add exercicio2

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿int idade, contador;
+contador = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("digite a idade");
+    idade = int.Parse(Console.ReadLine());
+    if (idade >= 18)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine(contador + " pessoas sao maiores de idade ");
+
