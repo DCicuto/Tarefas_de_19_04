c0bc8879422461891b87599d8dfb39cd01b38a98
Daiane Costa <cicuto.adv@gmail.com>
2024-04-21 23:09:41 -0300

    add exercicio1

---
+++ exercicio1_numeros_primos/exercicio1_numeros_primos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+int n,divisor;
+Console.WriteLine("Digite um numero");
+n = int.Parse(Console.ReadLine());
+divisor = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    if (n % n == 0)
+    {
+        divisor++;
+    }
+}
+if (divisor == 2)
+{
+    Console.WriteLine("Numero primo");
+}
+else
+{
+    Console.WriteLine("numero nao primo"); 
+}
+  
